5482a9b44d1356ad53a52cc0728232ce9393ad69
vigneshwar-creates <jvigneshwar.work@gmail.com>
2026-05-26 13:14:03 +0530

    Add landing page components and styling

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React, { useState } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
+import Landing from './Landing.jsx';
+import './index.css';
+
+function RootComponent() {
+  const [showWorkspace, setShowWorkspace] = useState(false);
+
+  return showWorkspace ? (
+    <App />
+  ) : (
+    <Landing onEnter={() => setShowWorkspace(true)} />
+  );
+}
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+  <React.StrictMode>
+    <RootComponent />
+  </React.StrictMode>
+);
